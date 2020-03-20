9a5e58d3ffa3138830a822d5e268d9cc45b36e63
Rustam Ibragimov <iamdaiver@gmail.com>
2020-03-20 07:14:04 +0300

    [instrumentation] prepare for Siege::Tools (f*)

---
+++ lib/siege/tools.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+# @api private
+# @since 0.1.0
+module Siege::Tools
+  require_relative 'tools/instrumentation'
+end
---
+++ lib/siege/tools/instrumentation.rb
@@ -0,0 +1,6 @@
+# frozen_string_literal: true
+
+# @api private
+# @since 0.1.0
+module Siege::Tools::Instrumentation
+end
